62500ce47316a5415812e0d56628b75c69c9ebef
Angel Coco Chen <angel@nuisance.co>
2019-04-17 23:49:25 +0800

    App setup (#5)
    * Initialise app with create-react-app
    
    * Replaces default content with Helix content and simple styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <p>Helix QA Tool</p>
+        </header>
+        <span>Let's get this show on the road!</span>
+      </div>
+    );
+  }
+}
+
+export default App;
